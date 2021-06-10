e33659e2c45bf470c752cb660bcf8a0e490e7fe2
Ryan2103 <84386601+Ryan2103@users.noreply.github.com>
2021-06-10 16:05:42 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,37 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 100
+        }
+        
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+           
+            push ()
+            stroke(48,22,8)
+          
+                strokeWeight(10);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+
+            pop ()
+    
+        }
+        
+    }
+}
